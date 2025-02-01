3d93f3847ac63d6689f98baba2fcea0c48b1e09d
Nugroho Arief Widodo <nawidodo@gmail.com>
2025-02-01 17:49:06 +0700

    Initial Commit

---
+++ tinyPOS/tinyPOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  tinyPOSApp.swift
+//  tinyPOS
+//
+//  Created by nugroho.widodo on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct tinyPOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tinyPOSTests/tinyPOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  tinyPOSTests.swift
+//  tinyPOSTests
+//
+//  Created by nugroho.widodo on 01/02/25.
+//
+
+import Testing
+@testable import tinyPOS
+
+struct tinyPOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
